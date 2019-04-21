aad33151e4bcc9bd284bff8703f6c58c00a86810
roacult <roiacult27@gmail.com>
2019-04-22 00:13:55 +0100

    presentation library

--- jstarter_domain/src/main/java/com/roacult/kero/team7/jstarter_domain/exception/Failure.kt
+++
@@ -1,9 +0,0 @@
-package com.roacult.kero.team7.jstarter_domain.exception
-
- /**
-  * base class of all possible failures
-  * extend this class create your own type
-  * of fails
-  * */
-
-open class Failure
