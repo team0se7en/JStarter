14e0dde92f22adc2256e71831ec691c17a698902
roacult <roiacult27@gmail.com>
2019-05-21 12:35:41 +0100

    add presentation lib

---
+++ jstarter_presentation/src/main/java/com/roacult/kero/team7/jstarter_presentation/schedulers/AppCoroutineDispatchers.kt
@@ -0,0 +1,13 @@
+package com.roacult.kero.team7.jstarter_presentation.schedulers
+
+import com.roacult.kero.team7.jstarter_domain.functional.CouroutineDispatchers
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+class AppCoroutineDispatchers  : CouroutineDispatchers {
+    override val io: CoroutineDispatcher = Dispatchers.IO
+    @ExperimentalCoroutinesApi
+    override val computaion: CoroutineDispatcher = Dispatchers.Unconfined
+    override val main: CoroutineDispatcher = Dispatchers.Main
+}
